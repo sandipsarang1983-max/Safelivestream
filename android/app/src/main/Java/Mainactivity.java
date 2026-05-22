afb9c482f32c595e8543aa99541b4d0256bca232
sandipsarang1983-max <sandipsarang1983@gmail.com>
2026-05-22 07:30:14 +0530

    Create Mainactivity.java

---
+++ android/app/src/main/Java/Mainactivity.java
@@ -0,0 +1,7 @@
+package com.example.safe_live_stream_app; // Use your actual package name
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // Keep this completely empty
+}
